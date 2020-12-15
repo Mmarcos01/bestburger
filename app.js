a1d65eff105dac67865a533966759986e88eae7f
Marie Marcos <marie.marcos25@gmail.com>
2020-12-14 22:48:15 -0800

    to learn functions, grouped like items into functions

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 console.log("Welcome!");
 
-var promptQuestion = "Do you like burgers?";
-var response = prompt(promptQuestion);
-console.log(response);
+var response;
+
+function getUserPrompt(){
+	var promptQuestion = "Do you like burgers?";
+	response = prompt(promptQuestion);
+	console.log(response);
+}
+getUserPrompt();
 
 var alertMessage;
 var color;
 
-if (response === "yes") {
+function giveReply(response) {
+	
+	if (response === "yes") {
 	alertMessage = "You're in the right place!";
 	color= "#00FF00";
 } else if (response === "no") {
@@ -16,11 +23,30 @@ if (response === "yes") {
 } else {
 	alertMessage = "That's awesome!";
 	color= "#0000FF";
-}
-    
+}}
+
+giveReply(response);
+
 alert(alertMessage);
 
 var element = document.getElementById('response-output')
 
 element.innerText = alertMessage;
 element.style.color = color;
+
+var color;
+
+function setBackgroundColorByInput(resp) {
+
+ if (resp === "yes") {
+	color = "beige";
+} else if (resp === "no") {
+	color = "lightpink";
+} else {
+	color = "grey";
+}
+
+document.getElementById("content").style.backgroundColor = color;
+}
+
+setBackgroundColorByInput(resp);
