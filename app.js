3208fec37177524ad03dc39ded490ccbab78f39c
Marie Marcos <marie.marcos25@gmail.com>
2020-12-15 23:16:56 -0800

    for practice, tried loops

--- app.js
+++ app.js
@@ -10,45 +10,38 @@ function validateUserPassword() {
 		userPassword = prompt('input password');
 	}
 }
-	
-// do while is guaranteed to execute at least once
-// ====================
 
 function getUserPrompt(){
 	var promptQuestion = "Do you like burgers?";
 	response = prompt(promptQuestion);
 }
 
-function giveReply(response) {
-	
-  if (response === "yes") {
-	alertMessage = "You're in the right place!";
-	color= "white";
-} else if (response === "no") {
-	alertMessage = "Stick around for your burger-loving friends.";
-	color= "#00FF00";
-} else {
-	alertMessage = "That's awesome!";
-	color= "#0000FF";
-}}
+function giveReply(response) {	
+	if (response === "yes") {
+		alertMessage = "You're in the right place!";
+		color= "white";
+	} else if (response === "no") {
+		alertMessage = "Stick around for your burger-loving friends.";
+		color= "#00FF00";
+	} else {
+		alertMessage = "That's awesome!";
+		color= "#0000FF";
+	}
 
-giveReply(response);
+	var element = document.getElementById('response-output');
 
-var element = document.getElementById('response-output')
-
-element.innerText = alertMessage;
-element.style.color = color;
-
-//var colorOnResponse;
+	element.innerText = alertMessage;
+	element.style.color = color;
+}
 
 function setColorBasedOnUserInput(rsp) {
 
- if (rsp === "yes") {
-	color = "black";
-} else if (rsp === "no") {
-	color = "grey";
-} else {
-	color = "brown";
+	if (rsp === "yes") {
+		color = "black";
+	} else if (rsp === "no") {
+		color = "grey";
+	} else {
+		color = "brown";
 }
 
 document.getElementById("content").style.backgroundColor = color;
@@ -56,16 +49,21 @@ document.getElementById("content").style.backgroundColor = color;
 
 var elements = document.getElementById("content")
 
-
-
-
-// 0-based indexing
-for (var i = 0; i < 10; i++) {
-	//body of for loop
-	console.log(i);
-}
+//for (var i = 0; i < 10; i++) {
+//	console.log(i);
+//}
 
 validateUserPassword();
 getUserPrompt();
+giveReply(response);
 alert(alertMessage);
 setColorBasedOnUserInput(response); //calls the OUTSIDE variable of 'response' above
+
+//var numberGiven = getUserPrompt("How many burgers do you want?");
+//for (var i = 0; i < numberGiven; i++) {
+
+	//console.log(i);
+	//addBurgToDocument("hamburglar");
+
+//function addImage() {
+	//var img
